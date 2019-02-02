87a6cf517dc55de6452edb44e017998b2c91958d
Luis Ushiña <guffenix@gmail.com>
2019-02-02 12:20:46 -0500

    update pagos.service, posicion consolidada

--- src/app/pos-consolidada/pos-consolidada.component.ts
+++ src/app/pos-consolidada/pos-consolidada.component.ts
@@ -87,4 +87,9 @@ export class PosConsolidadaComponent implements OnInit {
     this.obtenerListaPrestamos();
   }
 
+  toDate(_miString){
+    let patron = "[UTC]";
+    let nuevoValor    = "";
+    return _miString.replace(patron, nuevoValor);
+  }
 }
